7172d268335e9accde47a2abe36197c789cc66a3
aldaxho <65668485+aldaxho@users.noreply.github.com>
2025-11-10 14:34:19 -0400

    para production

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -1,8 +1,8 @@
 // Configuración simple de la API
 const API_CONFIG = {
   // URL base del backend - CAMBIAR AQUÍ PARA CAMBIAR EL SERVIDOR
-  BASE_URL: 'http://localhost:3001',//'https://157-245-1-74.sslip.io',
-  WS_URL: 'ws://localhost:3001',//'wss://157-245-1-74.sslip.io',
+  BASE_URL: 'https://138-197-38-159.sslip.io',//'https://157-245-1-74.sslip.io',
+  WS_URL: 'wss://138-197-38-159.sslip.io',//'wss://157-245-1-74.sslip.io',
   
   // Función helper para construir URLs completas
   getUrl: (endpoint) => `${API_CONFIG.BASE_URL}${endpoint}`,
